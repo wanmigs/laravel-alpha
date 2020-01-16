41a8c1d280249d22c1bb930cae6b9ea6a352d10c
Thomas John Gulisao <thomasjohn.gulisao@fligno.com>
2020-01-16 14:12:52 +0800

    Remove email/verify to default web routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,4 +2,3 @@
 
 Route::view('login', 'Auth::login');
 Route::view('register', 'Auth::register');
-Route::view('email/verify', 'Auth::verification')->name('verification.verify')->middleware('signed');
